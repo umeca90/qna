a67cab6e3a067552200269fda4dc094c2945b435
umeca90 <umeca90@gmail.com>
2019-04-10 16:45:32 +0300

    Implementing user-story "Author can edit his question with attachments"

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -46,6 +46,22 @@ feature 'User can edit his question', %q{
 
       expect(page).to have_content "Title can't be blank"
     end
+
+    scenario 'edits his qoestion with attachments' do
+      within '.question' do
+        fill_in 'Title', with: 'new title'
+        fill_in 'Body', with: 'new body'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save question'
+
+        expect(page).to_not have_content question.title
+        expect(page).to_not have_content question.body
+        expect(page).to have_content 'new title'
+        expect(page).to_not have_selector 'textarea'
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   describe 'Authenticated user as not author', js: true do
