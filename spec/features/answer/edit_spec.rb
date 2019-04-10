773ff5fe73522e397281e63f87afdb14d8396172
umeca90 <umeca90@gmail.com>
2019-04-10 16:54:00 +0300

    Implementing user-story "Author of the answer able to edit it with attachments"

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -46,6 +46,18 @@ feature 'User can edit his answer', %q{
       end
     end
 
+    scenario 'as an author edits his answer wtih attachments', js: true do
+      within '.answers' do
+        click_on 'Edit'
+        fill_in 'Your answer', with: :edit_body
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
+
     scenario "tries to edit other user's question", js: true do
       within(".answer_#{answer2.id}") do
         expect(page).to have_content(answer2.body)
