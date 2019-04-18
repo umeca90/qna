063f418033a442e149dc7299904862b06e632729
umeca90 <umeca90@gmail.com>
2019-04-18 18:04:42 +0300

    Create Comment model with associations and test it

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
 
   has_many :answers, foreign_key: 'author_id', dependent: :destroy
   has_many :questions, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :awards, dependent: :destroy
   has_many :votes, dependent: :destroy
 
   def author_of?(object)
     object.author_id == id
   end
-  
 end
