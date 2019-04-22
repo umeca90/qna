a5be2294f4130f8e8fa052c96d66ff98e30c46f3
umeca90 <umeca90@gmail.com>
2019-04-22 19:12:44 +0300

    Finishing "User.find_for_oath" method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,30 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :awards, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
+
+  def self.find_for_oauth(auth)
+    authoriation = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authoriation.user if authoriation
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+
+    user
+  end
 
   def author_of?(object)
     object.author_id == id
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
   it { should have_many(:awards).dependent(:destroy) }
   it { should have_many(:votes).dependent(:destroy) }
   it { should have_many(:comments).dependent(:destroy) }
+  it { should have_many(:authorizations).dependent(:destroy) }
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
@@ -13,6 +14,76 @@ RSpec.describe User, type: :model do
   let!(:user1) { create :user }
   let!(:user2) { create :user }
   let!(:question) { create :question, author: user1 }
+
+  describe '.find_for_oauth' do
+    let!(:user) { create :user }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+
+    context 'user already has authorization' do
+      it 'returns the user' do
+        user.authorizations.create(provider: 'facebook', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+
+    context 'user has not authorization' do
+      context 'user already exists' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: user.email }) }
+
+        it 'does not creates new user' do
+          expect { User.find_for_oauth(auth) }.to_not change(User, :count)
+        end
+
+        it 'creates authorization for user' do
+          expect { User.find_for_oauth(auth) }.to change(user.authorizations, :count).by 1
+        end
+
+        it 'creates authorization with provider and uid' do
+          authorization = User.find_for_oauth(auth).authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+
+        it 'returns the user' do
+          expect(User.find_for_oauth(auth)).to eq user
+        end
+      end
+
+      context 'user does not exists' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'new@email.com' }) }
+
+        it 'creates new user' do
+          expect { User.find_for_oauth(auth) }.to change(User, :count).by 1
+        end
+
+        it 'returns new user' do
+          expect(User.find_for_oauth(auth)).to be_a User
+        end
+
+        it 'fills user email' do
+          user = User.find_for_oauth(auth)
+
+          expect(user.email).to eq auth.info[:email]
+        end
+
+        it 'creates authorization for user' do
+          user = User.find_for_oauth(auth)
+
+          expect(user.authorizations).to_not be_empty
+        end
+
+        it 'creates authorization with provider and uid' do
+          authorization = User.find_for_oauth(auth).authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+
+      end
+    end
+
+  end
 
   describe 'User#author_of?' do
     it 'confirms that user is the author' do
