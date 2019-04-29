685ca78dd77561d9b76f3a61b3300ab927cb48a7
umeca90 <umeca90@gmail.com>
2019-04-29 14:34:02 +0300

    Implement Questions_Api via doorkeeper and serializer

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ gem 'omniauth-vkontakte'
 gem 'omniauth-instagram'
 gem 'cancancan'
 gem 'doorkeeper'
+gem 'active_model_serializers', '~> 0.10'
+gem 'oj'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -3,3 +3,65 @@ require 'rails_helper'
 describe 'Questions API', type: :request do
   let(:headers) { { "CONTENT_TYPE" => "application/json",
                     "ACCEPT" => "application/json" } }
+
+  describe 'GET /api/v1/questions' do
+    context 'unauthorized' do
+      it 'returns 401 if there is no access_token' do
+        get '/api/v1/questions', headers: headers
+        expect(response.status).to eq 401
+      end
+      it 'returns 401 if access_token is invalid' do
+        get '/api/v1/questions', params: { access_token: '1234' }, headers: headers
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:user) { create :user }
+      let(:access_token) { create(:access_token) }
+      let!(:questions) { create_list(:question, 2, author: user) }
+      let(:question) { questions.first }
+      let(:question_response) { json['questions'].first }
+      let!(:answers) { create_list(:answer, 3, author: user, question: question) }
+
+      before { get '/api/v1/questions', params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns list of questions' do
+        expect(json['questions'].size).to eq 2
+      end
+
+      it 'reterns all public fields' do
+        %w[id title body created_at updated_at].each do |attr|
+          expect(question_response[attr]).to eq question.send(attr).as_json
+        end
+      end
+
+      it 'contains user object' do
+        expect(question_response['author']['id']).to eq question.author.id
+      end
+
+      it 'contains short title' do
+        expect(question_response['short_title']).to eq question.title.truncate(7)
+      end
+
+      describe 'answers' do
+        let(:answer) { answers.first }
+        let(:answer_response) { question_response['answers'].first }
+
+        it 'returns list of answers' do
+          expect(question_response['answers'].size).to eq 3
+        end
+
+        it 'reterns all public fields' do
+          %w[id body author_id created_at updated_at].each do |attr|
+            expect(answer_response[attr]).to eq answer.send(attr).as_json
+          end
+        end
+      end
+    end
+  end
+end
