0cd23f1f921b24f471aa519af5d82c5e5061d637
umeca90 <umeca90@gmail.com>
2019-05-30 17:21:30 +0300

    Finishing deploy part one

--- Capfile
+++ Capfile
@@ -8,6 +8,8 @@ require "capistrano/bundler"
 require "capistrano/rails"
 require "capistrano/passenger"
 require "capistrano/sidekiq"
+require "thinking_sphinx/capistrano"
+require "whenever/capistrano"
 
 
 # Load the SCM plugin appropriate to your project:
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "192.168.0.21", user: "deployer", roles: %w{app db web}, primary: true
+server "165.22.193.242", user: "deployer", roles: %w{app db web}, primary: true
 set :rail_env, :production
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
